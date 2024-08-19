82fc1da8e6b7b1ef6bb4cf04002edba2cc54d498
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-19 12:42:13 +0330

    - Update User Interface

--- Factorial/Module1.vb
+++ Factorial/Module1.vb
@@ -6,24 +6,33 @@ Module Module1
         Dim n As Integer
         Dim factorial As Long = 1
 
+        System.Console.ForegroundColor = ConsoleColor.DarkCyan
         ' Prompt the user to enter a non-negative integer.
-        Console.Write("Please enter a non-negative integer : ")
+        System.Console.Write("Please enter a non-negative integer : ")
 
+        System.Console.ForegroundColor = ConsoleColor.Yellow
         ' Read the user's input and convert it to an integer.
-        n = Convert.ToInt32(Console.ReadLine())
+        n = System.Convert.ToInt32(System.Console.ReadLine())
+        System.Console.ForegroundColor = ConsoleColor.Magenta
+        System.Console.WriteLine("-----------------------")
 
         ' Check if the input is valid (non-negative).
         If n < 0 Then
-            Console.WriteLine("The entered number must be non-negative.")
+            System.Console.BackgroundColor = ConsoleColor.Red
+            System.Console.ForegroundColor = ConsoleColor.White
+            System.Console.WriteLine("The entered number must be non-negative.")
+            System.Console.ResetColor()
         Else
             ' Calculate the factorial using a loop.
             For i As Integer = 1 To n
                 factorial *= i
             Next
-
+            System.Console.BackgroundColor = ConsoleColor.Blue
+            System.Console.ForegroundColor = ConsoleColor.White
             ' Display the result.
-            Console.WriteLine($"The factorial of {n} is: {factorial}")
+            System.Console.WriteLine($"The factorial of {n} is: {factorial}")
         End If
+        System.Console.ResetColor()
 
         ' Programmer Inforamtion and GitHub Username
         System.Console.ForegroundColor = ConsoleColor.Magenta
@@ -32,7 +41,7 @@ Module Module1
         System.Console.WriteLine("Programmer : Amin Mirzaei - Github(UserName) : AminMirzaeiOne")
 
         ' Pause the console to keep it open.
-        Console.ReadKey()
+        System.Console.ReadKey()
     End Sub
 
 End Module
